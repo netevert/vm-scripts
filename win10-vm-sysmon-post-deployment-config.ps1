442966c36e466f242c2c1c6d6bb1c5524e3830d2
netevert <netevert@protonmail.com>
2022-05-27 14:21:31 +0200

    fixes and improvements

--- win10-vm-sysmon-post-deployment-config.ps1
+++ win10-vm-sysmon-post-deployment-config.ps1
@@ -117,7 +117,8 @@ $appname = "Mail"
 ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true}
 
 (New-Object System.Net.WebClient).DownloadFile($configfolderlocation, "$env:USERPROFILE\Desktop\files.zip")
-7z e$env:USERPROFILE\Desktop\files.zip -o$env:USERPROFILE\Desktop\files
+
+7z x "$env:USERPROFILE\Desktop\files.zip" -o"$env:USERPROFILE\Desktop\"
 
 Add-Type -TypeDefinition @'
 using System.Runtime.InteropServices;
