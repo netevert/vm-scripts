4b06ff4235ab29b7db9fb6ca84831481c5e79a55
netevert <netevert@protonmail.com>
2022-05-27 16:24:27 +0200

    fixes and improvements

--- win10-vm-sysmon-post-deployment-config.ps1
+++ win10-vm-sysmon-post-deployment-config.ps1
@@ -122,21 +122,21 @@ $appname = "Mail"
 7z x "C:\files.zip" -o"C:\"
 
 # Set desktop background
-Add-Type -TypeDefinition @'
-using System.Runtime.InteropServices;
-public class Wallpaper {
-    public const uint SPI_SETDESKWALLPAPER = 0x0014;
-    public const uint SPIF_UPDATEINIFILE = 0x01;
-    public const uint SPIF_SENDWININICHANGE = 0x02;
-    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-    private static extern int SystemParametersInfo (uint uAction, uint uParam, string lpvParam, uint fuWinIni);
-    public static void SetWallpaper (string path) {
-        SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
-    }
-}
-'@
+# Add-Type -TypeDefinition @'
+# using System.Runtime.InteropServices;
+# public class Wallpaper {
+#     public const uint SPI_SETDESKWALLPAPER = 0x0014;
+#     public const uint SPIF_UPDATEINIFILE = 0x01;
+#     public const uint SPIF_SENDWININICHANGE = 0x02;
+#     [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+#     private static extern int SystemParametersInfo (uint uAction, uint uParam, string lpvParam, uint fuWinIni);
+#     public static void SetWallpaper (string path) {
+#         SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+#     }
+# }
+# '@
 
-[Wallpaper]::SetWallpaper("C:\files\desktop.PNG")
+# [Wallpaper]::SetWallpaper("C:\files\desktop.PNG")
 
 # Move companion folder and README to C: folder
 Move-Item C:\files\companion_folder C:\companion_folder
